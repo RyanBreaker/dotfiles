c39676d7913d169338496e64cacf82c6d7bf9154
Ryan Breaker <ryan@breaker.rocks>
2023-08-02 10:55:40 -0500

    Create plugins.lua

---
+++ nvim/lua/custom/plugins.lua
@@ -0,0 +1,79 @@
+local plugins = {
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "rust-analyzer",
+        "prettier",
+        "html-lsp",
+        "stylua",
+        "djlint",
+      },
+    },
+  },
+  {
+    "simrat39/rust-tools.nvim",
+    ft = "rust",
+    dependencies = "neovim/nvim-lspconfig",
+    opts = function()
+      return require "custom.configs.rust-tools"
+    end,
+    config = function(_, opts)
+      require("rust-tools").setup(opts)
+    end,
+  },
+  {
+    "mfussenegger/nvim-dap",
+    init = function()
+      require("core.utils").load_mappings("dap")
+    end
+  },
+  {
+    "saecki/crates.nvim",
+    ft = { "toml" },
+    config = function(_, opts)
+      local crates = require "crates"
+      crates.setup(opts)
+      require("cmp").setup.buffer {
+        sources = { { name = "crates" } },
+      }
+      crates.show()
+      require("core.utils").load_mappings("crates")
+    end,
+  },
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function()
+      vim.g.rustfmt_autosave = 1
+    end,
+  },
+  {
+    "theHamsta/nvim-dap-virtual-text",
+    lazy = false,
+    config = function(_, opts)
+      require("nvim-dap-virtual-text").setup()
+    end
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function()
+      local cmp = require("cmp")
+      local M = require("plugins.configs.cmp")
+      M.completion.completeopt = "menu,menuone,noselect"
+      M.mapping["<CR>"] = cmp.mapping.confirm({ select = true }),
+      table.insert(M.sources, {name = "cratest"})
+      return M
+    end
+  },
+  {
+    "Canop/nvim-bacon",
+    ft = { "rust" },
+  },
+}
+
+return plugins
